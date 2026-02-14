45bf274e6c904283311fcf5aab88132903e35ca2
Vibodh <mvibodhk@gmail.com>
2026-02-14 16:28:41 +0000

    strip one property

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -73,7 +73,7 @@ const Create = () => {
     const subjects = subjectsQuery?.data?.data || [];
     const subjectsLoading = subjectsQuery.isLoading;
 
-    const teachers = teachersQuery?.data?.data || [];
+    const teachers = teachersQuery?.data? || [];
     const teachersLoading = teachersQuery.isLoading;
 
     const bannerPublicId = form.watch('bannerCldPubId')
